82b6e308a4cfde2fc6272d1f1ba27a5e8e5a0e19
Xavier Campillo <xavicn@me.com>
2020-07-12 17:35:23 +0200

    Including Bootstrap and initial layout and styles for App component

--- src/App.js
+++ src/App.js
@@ -20,10 +20,29 @@ class App extends Component {
 
   // Renders
   render() {
+    const { articles } = this.state;
+
     return (
       <div className="App">
         <h2>Billin code challenge</h2>
-        <pre>{JSON.stringify(this.state.articles, null, 2)}</pre>
+
+        <div className="row">
+          { articles.map((article) =>
+
+              <div className="col-12 col-md-6 col-lg-4 col-xl-3" key={ article.id }>
+                <div className="card m-3">
+                  <div className="card-body">
+                    <h5 className="card-title">{ article.title }</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{ article.author }</h6>
+                    <p className="card-text">{ article.excerpt }</p>
+                  </div>
+                </div>
+              </div>
+
+            )
+          }
+        </div>
+
       </div>
     );
   }
